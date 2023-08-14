6ea7b59e4d4c2ab753fa5c0c3787b06f895372c1
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-08-14 14:20:28 +0100

    add example of each function

--- src/index.js
+++ src/index.js
@@ -60,27 +60,73 @@ const linkedList = () => {
 			}
 		}
 	};
-	const pop = () => {};
-	const contains = (value) => {};
-	const find = (value) => {};
-	const toString = () => {
+	const pop = () => {
+		const temp = at(length - 1);
+		length -= 1;
+		temp.nextNode = null;
+	};
+	const contains = (value) => {
 		let temp = headNode;
+		let found = false;
 		for (let i = 1; i <= length; i += 1) {
-			console.log(temp.value);
+			if (temp.value === value) {
+				found = true;
+			}
 			temp = temp.nextNode;
 		}
+		return found;
+	};
+	const find = (value) => {
+		let temp = headNode;
+		let indexFound = "Value not found";
+		for (let i = 1; i <= length; i += 1) {
+			if (temp.value === value) {
+				indexFound = `Value "${temp.value}" is at index ${i}`;
+			}
+			temp = temp.nextNode;
+		}
+
+		return indexFound;
+	};
+	const toString = () => {
+		let temp = headNode;
+		let nodeList = "";
+
+		for (let i = 1; i <= length; i += 1) {
+			nodeList += `(${temp.value})`;
+			if (i !== length) {
+				nodeList += " -> ";
+			}
+			temp = temp.nextNode;
+		}
+		return nodeList;
 	};
 
-	return { append, prepend, toString, size, head, tail, at };
+	return {
+		append,
+		prepend,
+		toString,
+		size,
+		head,
+		tail,
+		at,
+		pop,
+		contains,
+		find,
+	};
 };
 
 const list = linkedList();
 
 list.append("a");
-list.append("b");
-list.append("c");
-list.append("d");
-list.append("e");
-list.append("f");
-
-console.log(list.at(5));
+console.log(list.toString()); // a
+list.prepend("b");
+console.log(list.toString()); // (b) -> (a)
+console.log(list.size()); // 2
+console.log(list.head()); // {value: b nextNode: {value:a nextNode: null}}
+console.log(list.tail()); // {value:a nextNode: null}
+console.log(list.at(2)); // {value:a nextNode: null}
+list.pop();
+console.log(list.toString()); // (b)
+console.log(list.contains("a")); // false
+console.log(list.find("b")); // Value "b" is at index 1
